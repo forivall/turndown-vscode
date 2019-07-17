5d5d76ea77d011e5d9f0fa1bc365923d7a7d0b6f
Emily Marigold Klassen <forivall@gmail.com>
2019-07-17 13:26:13 -0700

    feat: support multiple selection and automatically add prefix
    in particular, i wanted the prefix solution for converting html in
    docstrings in autogenerated types into markdown

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,24 @@
 'use strict'
 import * as vscode from 'vscode'
-const TurnDown = require('turndown')
+import TurnDown = require('turndown')
 
 export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('extension.turndown',() => {
 			const editor = vscode.window.activeTextEditor
 			editor.edit((edit) => {
-                const t = vscode.window.activeTextEditor.document.getText(editor.selection)
                 const config = vscode.workspace.getConfiguration('turndown')
-				edit.replace(editor.selection, replaceText(t, config.get('useSpecialImage')))
+                const doc = vscode.window.activeTextEditor.document
+
+                const td = createTurndown(config.get('useSpecialImage'))
+                for (const sel of editor.selections) {
+                    const t = doc.getText(sel)
+                    const prefix = doc.getText(new vscode.Range(
+                        new vscode.Position(sel.start.line, 0),
+                        sel.start
+                    ))
+                    const replacement = td.turndown(t).replace(/\n/g, () => `\n${prefix}`)
+                    edit.replace(sel, replacement)
+                }
 			})
 		},
 	)
@@ -20,16 +30,20 @@ export function deactivate() {
 
 }
 
-export function replaceText(text: string, specialImage: boolean) {
+function createTurndown(useSpecialImage: boolean) {
     const td = new TurnDown()
-    if(specialImage) {
+    if (useSpecialImage) {
         td.addRule('image', {
             filter: 'img',
-            replacement: function(content, node) {
+            replacement: function(content, node: HTMLElement) {
                 const rv = '{{' + node.outerHTML + '}}'
                 return rv
             },
         })
     }
-	return td.turndown(text)
+    return td
+}
+
+export function replaceText(text: string, useSpecialImage: boolean) {
+    return createTurndown(useSpecialImage).turndown(text)
 }
